4d628507dc3f055207d5d58ae3953d42d109bbc0
Jake <jakeor45@gmail.com>
2024-08-19 11:02:20 -0700

    Creating a deck of cards

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ struct Deck {
 }
 
 fn main() {
-    let deck = Deck {cards: vec![]};
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+    let mut deck = Deck {cards: vec![]};
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            deck.cards.push(card);
+        }
+    }
     
-    println!("Here's your deck: {:?}", deck);
+    println!("Here's your deck: {:#?}", deck);
 }
