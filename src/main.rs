0ca192cd2c9b0a9f9494e5ed884c26f8ee027a54
Jake <jakeor45@gmail.com>
2024-08-19 11:09:34 -0700

    refactoring- breaking logic out into impl.

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ struct Deck {
     cards: Vec<String>,   
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
-
-    let mut deck = Deck {cards: vec![]};
-
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            deck.cards.push(card);
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+    
+        let mut deck = Deck {cards: vec![]};
+    
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                deck.cards.push(card);
+            }
         }
+        deck
     }
+}
+
+fn main() {
+    let deck = Deck::new();
     
     println!("Here's your deck: {:#?}", deck);
 }
