aa7147c2adf4065c37c30a7b61f26e5ba2c800a0
jacob-oreilly <jakeor45@gmail.com>
2024-09-05 20:47:36 -0700

    Finishing Section 2 of the rust course

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,16 @@ impl Deck {
         let mut rng = thread_rng();
         self.cards.shuffle(&mut rng);
     }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 
 fn main() {
     let mut deck = Deck::new();
     deck.shuffle();
-    println!("Here's your deck: {:#?}", deck);
+    let cards = deck.deal(3);
+    println!("Here's your hand: {:#?}", cards);
+    println!("Here's what's left in the deck: {:#?}", deck);
 }
